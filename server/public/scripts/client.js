aa7f8e589b95bfbeecd7d30e76501bdb51ae2106
TuckerLandis <landistuckerc@gmail.com>
2021-05-20 13:55:41 -0500

    init commit bit late

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,46 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+
+  generateRand();
+
+  // click listeners
+  $('#submitGuesses').on('click', handleSubmit);
+}
+
+function handleSubmit(){
+  console.log('button clicked');
+
+  let guessesObject = {
+    player1Guess: $('#player1In').val(),
+    player2Guess: $('#player2In').val(),
+    player3Guess: $('#player3In').val(),
+    player4Guess: $('#player4In').val(),
+  }
+  console.log(guessesObject);
+  
+
+   $.ajax({
+     url: '/guess',
+     method: 'POST',
+     data: guessesObject //becomes req.body on the server
+   }).then(response =>{
+      console.log(response);
+   })
+     .catch(function (error) {
+         console.log('error')
+         alert('something went wrong with Post, try again')
+})
+}
+
+function generateRand() {
+  console.log('Generating Number');
+
+    $.ajax({
+      url:'/random-num',
+      method: 'GET'
+    }).then(response => {
+          console.log(response);
+  
+    })
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+const guessArray = require('./modules/guess.js');
+const randomNum = require('./modules/random-num');
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -11,6 +14,15 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+app.post('/guess', (req, res) =>{
+console.log(req.body);
+res.sendStatus(200);
+})
+
+app.get('/random-num', (req, res) =>{
+  console.log('got to /random-num');
+  res.send(randomNum);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
