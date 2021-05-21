4f43c8d119b61f3fd18a8b753bf65e52cc5e6458
TuckerLandis <landistuckerc@gmail.com>
2021-05-21 10:59:08 -0500

    functional for 1st guess

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,16 +1,25 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
-
+  // console.log("jquery is loaded!")
   generateRand();
-
+  // playAgain();
   // click listeners
   $('#submitGuesses').on('click', handleSubmit);
+  $('#resetButton').on('click', '#playAgainButton', resetNumbers)
+}
+
+let clickCount = 0;
+
+function resetNumbers(){
+  generateRand();
+  console.log('new random number??', generateRand());
 }
 
 function handleSubmit(){
   console.log('button clicked');
+
+  clickCounter();
 
   let guessesObject = {
     player1Guess: $('#player1In').val(),
@@ -20,8 +29,6 @@ function handleSubmit(){
   }
   console.log(guessesObject);
   
- 
-
    $.ajax({
      url: '/guess',
      method: 'POST',
@@ -32,11 +39,12 @@ function handleSubmit(){
      .catch(function (error) {
          console.log('error')
          alert('something went wrong with Post, try again')
+
 })
 handleCompare();
-
+//getGuesses();
 }
-
+//so we can see what the random number is. comment out before done.
 function generateRand() {
   console.log('Generating Number');
 
@@ -45,27 +53,118 @@ function generateRand() {
       method: 'GET'
     }).then(response => {
           console.log(response);
-  
     })
 }
 
 function handleCompare() {
   console.log('in handle compare');
-
   $.ajax({
     url: '/winner',
     method: 'GET'
   }).then(response => {
-    console.log(response);
+    console.log('in handle compare', response);
+    declareWinner(response);
     
   }).catch(function (error){
     console.log('errorHandleCompare'); 
   })
+
   } // end handleCompare
 
+function declareWinner (response){
 
+  let winners = '';
+  for (let i = 0; i<response.length; i++){
+    if (response[response.length-1].name == 'player1'){
+      winners = 'player 1';
+    }
+    if (response[response.length-1].name == 'player2'){
+      winners = 'player 2';
+    }
+    if (response[response.length-1].name == 'player3'){
+      winners = 'player 3';
+    }
+    if (response[response.length-1].name == 'player4'){
+      winners = 'player 4';
+    }
+  }
 
+  console.log('declare winner', winners);
+  $('#displayWinner').text(`${winners} is the winner!!`);
+}
   // get number to show up 
 
 
+  function clickCounter() {
+    clickCount++;
+    $('#guessCounter').text(`Total Guesses: ${clickCount}`)
   
+  }
+
+  // get guesshistory
+
+  // function getGuesses() {
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: '/guess'
+  //   }).then(function (response){
+  //     console.log(response)
+  //     //empty DOM
+  //     $('.historyBodySection').empty();
+
+  //     let comparison = [];
+
+  //     for (let i = 0; i < response.length; i++) {
+  //       if (response[i].player1Guess == randomNum) {
+  //         comparison[i].player1Guess.push('Winner');
+  //       } else if (response[i].player1Guess > randomNum) {
+  //         comparison[i].push('Too High');
+  //       } else if (response[i].player1Guess < randomNum) {
+  //         comparison[i].push('Too Low');
+  //       }
+  //       if (response[i].player1Guess == randomNum) {
+  //         comparison[i].player1Guess('Winner');
+  //       } else if (response[i].player1Guess > randomNum) {
+  //         comparison[i].push('Too High');
+  //       } else if (response[i].player1Guess < randomNum) {
+  //         comparison[i].push('Too Low');
+  //       }
+  //       if (response[i].player1Guess == randomNum) {
+  //         comparison[i].player1Guess('Winner');
+  //       } else if (response[i].player1Guess > randomNum) {
+  //         comparison[i].push('Too High');
+  //       } else if (response[i].player1Guess < randomNum) {
+  //         comparison[i].push('Too Low');
+  //       }
+  //       if (response[i].player1Guess == randomNum) {
+  //         comparison[i].player1Guess('Winner');
+  //       } else if (response[i].player1Guess > randomNum) {
+  //         comparison[i].push('Too High');
+  //       } else if (response[i].player1Guess < randomNum) {
+  //         comparison[i].push('Too Low');
+  //       }
+
+  //       $('.historyBodySection').append(
+  //         `<tr>
+  //         <td>${response[i].player1Guess}</td>
+  //         <td></td>
+  //         <td>${response[i].player2Guess}</td>
+  //         <td></td>
+  //         <td>${response[i].player3Guess}</td>
+  //         <td></td>
+  //         <td>${response[i].player4Guess}</td>
+  //         <td></td>
+  //         </tr>`
+  //       )
+  //     }
+  //   })
+  // }
+  
+
+
+  // function playAgain(){
+  //   console.log('clicked play again');
+  //   $('#resetButton').html(`
+  //   <button id="playAgainButton">Play Again!<button>
+  //   `)
+  // }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const PORT = 5000;
 const guessArray = require('./modules/guess.js');
 const randomNum = require('./modules/random-num.js');
 
+// let randomNumber = {
+//   randomNum: randomNum(1, 25)
+// };
+
+// function randomNum(min, max) {
+//   return Math.round(Math.random() * (max - min) + min);
+// }
+
+
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -27,8 +37,6 @@ app.get('/random-num', (req, res) =>{
   res.send(randomNum);
 })
 
-
-
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
@@ -37,33 +45,60 @@ app.listen(PORT, () => {
 function compareNumbers(guessArray) {
   // this should compare the guessObj values to the random number
   // do thing based on comparison (append)
-  // eventually animations "you win" type thing
+  // eventually animations "you win" type thing 
+  console.log('random number', randomNum);
   let winners = [];
   for (let i = 0; i < guessArray.length; i++) {
+// {
+
   //for (let guessObj of guessArray){
-    if(guessArray[i].player1Guess == randomNum){
+    if(guessArray[i].player1Guess == randomNum.randomNum){
         let winner = {
           name: 'player1',
         }
         console.log('should be winner object:', winner);
         winners.push(winner);
     }
-    else {
-      console.log('why wont it work');
+  
+    if(guessArray[i].player2Guess == randomNum.randomNum){
+      let winner = {
+        name: 'player2',
+      }
+      console.log('should be winner object:', winner);
+      winners.push(winner);
+  }
+    if(guessArray[i].player3Guess == randomNum.randomNum){
+      let winner = {
+        name: 'player3',
+      }
+      console.log('should be winner object:', winner);
+      winners.push(winner);
+  }
+  if(guessArray[i].player4Guess == randomNum.randomNum){
+    let winner = {
+      name: 'player4',
     }
+    console.log('should be winner object:', winner);
+    winners.push(winner);
+  }
+    // else {
+    //   // console.log('why wont it work');
+    //   // console.log('each guess', guessArray[i]);
+    //   // console.log('randon num is', randomNum)
+    // }
   }
   console.log('should be winners array', winners);
   
   return winners;
 }
 
-let compare = compareNumbers(guessArray);
+// let compare = compareNumbers(guessArray);
 
 app.get('/winner', (req, res) =>{
   console.log('at /winner');
-  console.log(compare);
+  console.log(compareNumbers(guessArray));
   
-  res.send(compare);
+  res.send(compareNumbers(guessArray));
 })
 
 // set variables to the key values of guesses object
